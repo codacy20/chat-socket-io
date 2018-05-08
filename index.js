4716560d35e2b546f532b0e37da98e9aed835d19
Remi Kristelijn <rkristelijn@gmail.com>
2018-05-08 14:40:19 +0200

    added some irc commands: /help /who /nick

--- index.js
+++ index.js
@@ -1,19 +1,49 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const channelBotPre = 'Channel bot: ';
+
+let nicks = [];
+function addNick(nick) {
+  nicks.push(nick);
+}
+function removeNick(nick) {
+  var index = nicks.indexOf(nick);
+  if (index > -1) {
+    nicks.splice(index, 1);
+  }
+}
+function renameNick(oldNick, newNick) {
+  removeNick(oldNick);
+  addNick(newNick);
+}
 
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function (socket) {
-  socket.broadcast.emit('hi');
-  console.log('a user connected');
+  if (!socket.name) socket.name = socket.id;
+  addNick(socket.name);
+  io.emit('channel message', channelBotPre + socket.name + ' connects. Welcome, type \'/nick user\' to change name.');
   socket.on('disconnect', function () {
-    console.log('user disconnected');
+    removeNick(socket.name);
+    io.emit('channel message', channelBotPre + socket.name + ' disconnects.');
   });
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  socket.on('chat message', function (msg) {
+    if (msg.substring(0, 5) === '/nick') {
+      let newName = msg.split(" ")[1];
+      io.emit('channel message', channelBotPre + socket.name + ' is now ' + newName);
+      renameNick(socket.name, newName);
+      socket.name = newName;
+    }
+    else if (msg.substring(0, 4) === '/who') {
+      io.emit('channel message', channelBotPre + 'in channel: ' + nicks.join(','));
+    } else if (msg.substring(0, 5) === '/help') {
+      io.emit('channel message', channelBotPre + '/help /who /nick');
+    } else {
+      io.emit('chat message', socket.name + ": " + msg);
+    }
   });
 });
 
